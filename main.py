ce3179427cc94d125d4ca728e67442cf242bc9a9
cutho99 <cuthoy@gmail.com>
2025-07-06 15:10:33 +0800

    create initial main & constant file

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame, constants
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
+
