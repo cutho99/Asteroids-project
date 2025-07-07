442a42e718e31a69293fa01d057af801ebb37501
cutho99 <cuthoy@gmail.com>
2025-07-07 21:43:39 +0800

    create a black screen

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame, constants
 
+SCREEN_WIDTH = constants.SCREEN_WIDTH
+SCREEN_HEIGHT = constants.SCREEN_HEIGHT
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))  #Set screen size
+    
+    while True:
+        for event in pygame.event.get():                    #Check if user close the window
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, "black")                #Fill screen w solid black
+        pygame.display.flip()                               #Refreshes the screen to update colour
 
 if __name__ == "__main__":
     main()
