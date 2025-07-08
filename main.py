ce1c74477e8955bc59c014c21dbae63587dfc79d
cutho99 <cuthoy@gmail.com>
2025-07-08 22:26:49 +0800

    created circleshape.py and child class  player.py, updated main.py to draw the player.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, constants
+import pygame, constants, player
 
 SCREEN_WIDTH = constants.SCREEN_WIDTH
 SCREEN_HEIGHT = constants.SCREEN_HEIGHT
@@ -10,15 +10,19 @@ def main():
     clock = pygame.time.Clock()                             #Create a clock object to track time
     dt = 0
 
+    player1 = player.Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+
     while True:
         for event in pygame.event.get():                    #Check if user close the window
             if event.type == pygame.QUIT:
                 return
             
         pygame.Surface.fill(screen, "black")                #Fill screen w solid black
+        player1.draw(screen)        
         pygame.display.flip()                               #Refreshes the screen to update colour
         clock.tick(60)                                      #Pauses the game loop for 1/60 second
         dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
